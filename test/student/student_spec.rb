56c4a98750bc70ac716757a51e744dbdbb57566d
rajeev <rajeevkushwaha1001@gmail.com>
2021-07-21 09:38:11 -0400

    updated studet spec file

--- test/student/student_spec.rb
+++ test/student/student_spec.rb
@@ -1,22 +1,42 @@
 require_relative 'student'
 RSpec.describe Student do
 
-	before do
-	  @student = Student.new("John","Doe") 
+	describe "instance methods" do 
+		subject { Student.new("John","Doe")}
+
+		it {respond_to(:first_name)}
+
+		it {respond_to(:last_name)}
+
+		it {respond_to(:student_fullname)}
 	end 
 
-	it "should respond to #first_name " do 
-		#student = Student.new("John","Doe")
-		expect(@student).to respond_to(:first_name)
-	end 
-
-	it "should respond to #last_name" do 
-		#student = Student.new("John","Doe")
-		expect(@student).to respond_to(:last_name)
-	end 
-
-	it "should respond to #student_fullname" do 
-		#student = Student.new("John","Doe")
-		expect(@student).to respond_to(:student_fullname)
+	describe "total number of students created " do 
+		it "should have student in total" do 
+			Student.new("john","doe")
+			Student.new("Jone","doe")
+			expect(Student.total_count).to eq(2)
+		end 
 	end 
 end 
+
+
+	# before do
+	#   @student = Student.new("John","Doe") 
+	# end 
+
+	# it "should respond to #first_name " do 
+	# 	#student = Student.new("John","Doe")
+	# 	expect(@student).to respond_to(:first_name)
+	# end 
+
+	# it "should respond to #last_name" do 
+	# 	#student = Student.new("John","Doe")
+	# 	expect(@student).to respond_to(:last_name)
+	# end 
+
+	# it "should respond to #student_fullname" do 
+	# 	#student = Student.new("John","Doe")
+	# 	expect(@student).to respond_to(:student_fullname)
+	# end 
+
